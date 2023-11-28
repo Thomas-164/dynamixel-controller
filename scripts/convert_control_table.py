f29a5420c3795f102227e7765af6d90b5ef612d4
gonespral <gonespral@me.com>
2023-11-28 16:07:41 +0100

    Convert control table

--- scripts/convert_control_table.py
+++ scripts/convert_control_table.py
@@ -6,7 +6,6 @@ def convert_header_to_json(header_file_path, json_file_path):
     # Regular expression to match C macro definitions
     macro_pattern = re.compile(r"#define\s+(\w+)\s+([0-9xXA-Fa-f]+)")
 
-    # Parse the header file
     with open(header_file_path, 'r') as file:
         content = file.read()
 
@@ -15,20 +14,28 @@ def convert_header_to_json(header_file_path, json_file_path):
 
     # Convert macros to JSON format
     json_data = {"Protocol_1": {"Control_Table": {}}}
+
     for name, value in macros:
+        bytelen = 1
+
         # Convert hexadecimal values to integers
         value = str(value)
         value = value.lower()
-        print(value)
         if value.startswith('0x') and len(value) <= 4:
             value = int(value, 16)
         elif len(value) <= 4:
             value = int(value)
         else:
-            value = value
+            value = value.upper()
 
-        # Add to JSON structure
-        json_data["Protocol_1"]["Control_Table"][name] = [value, 1]
+        # Check if suffix is L
+        if name.endswith('L'):
+            name = name[:-2]
+            bytelen = 2
+
+        if not name.endswith('H'):
+            # Add to JSON structure
+            json_data["Protocol_1"]["Control_Table"][name] = [value, bytelen]
 
     # Write the JSON data to a file
     with open(json_file_path, 'w') as json_file:
@@ -37,5 +44,5 @@ def convert_header_to_json(header_file_path, json_file_path):
 
 # Example usage
 header_file = '../3mxlControlTable.h'
-json_file = 'output_json_file.json'
+json_file = '../dynio/DynamixelJSON/3mxl.json'
 convert_header_to_json(header_file, json_file)
